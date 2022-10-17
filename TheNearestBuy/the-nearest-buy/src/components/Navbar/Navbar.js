76b83c1727a3f87c2f3e908b45468779c464e21e
Arjun Arora <arjunarora.aa.aa@gmail.com>
2022-10-18 01:14:38 +0530

    Added cards of products

--- TheNearestBuy/the-nearest-buy/src/components/Navbar/Navbar.js
+++ TheNearestBuy/the-nearest-buy/src/components/Navbar/Navbar.js
@@ -3,8 +3,10 @@ import logo from "../../logo.svg";
 import "./navbar.css";
 import { Button, Stack } from "@mui/material";
 import UserNavigation from "./UserNavigation";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(0);
   const [isLogin, setIsLogin] = useState(0);
   const login = () => {
     setIsLogin(1);
@@ -14,24 +16,33 @@ const Navbar = () => {
   };
 
   return (
-    <section id="navbar">
-      <img src={logo} alt="" />
-      <h2>Company Name</h2>
-      <div className="loginSignUp">
-        {isLogin ? (
-          <>
-            <UserNavigation isLogin={isLogin} setIsLogin={setIsLogin} />
-          </>
-        ) : (
-          <Stack spacing={2} direction="row">
-            <Button variant="contained" onClick={() => login()}>
-              Login
-            </Button>
-            <Button variant="outlined">Sign Up</Button>
-          </Stack>
-        )}
-      </div>
-    </section>
+    <>
+      <span className="navIcon" onClick={() => setIsNavOpen(!isNavOpen)}>
+        <MenuIcon />
+      </span>
+      {isNavOpen || window.innerWidth > 768 ? (
+        <section id="navbar">
+          <img src={logo} alt="" />
+          <h2>Company Name</h2>
+          <div className="loginSignUp">
+            {isLogin ? (
+              <div>
+                <UserNavigation isLogin={isLogin} setIsLogin={setIsLogin} />
+              </div>
+            ) : (
+              <Stack spacing={2} direction={{ xs: "column", sm: "row" }}>
+                <Button variant="contained" onClick={() => login()}>
+                  Login
+                </Button>
+                <Button variant="outlined">Sign Up</Button>
+              </Stack>
+            )}
+          </div>
+        </section>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
--- TheNearestBuy/the-nearest-buy/src/pages/home/Home.js
+++ TheNearestBuy/the-nearest-buy/src/pages/home/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
+import Product from "../../components/Products/Product";
+import "./home.css";
 
 const Home = () => {
+  fetch("http://127.0.0.1:8000/wel/?format=api", {
+    mode: "no-cors",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
-    <section>
-      <h1>Home</h1>
+    <section id="home">
+      <div className="products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </section>
   );
 };
